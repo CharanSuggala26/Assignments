926f37e959239ece2728d24e52b8ae9f7f977c28
CharanSuggala26 <suggalasaicharan789@gmail.com>
2025-07-20 22:05:06 +0530

    Promises & Promises Chain

--- Training/jstraining/functions.js
+++ Training/jstraining/functions.js
@@ -10,20 +10,52 @@
 //     });
 // }
 
-const getPromise = ()=>{
-    return new Promise((resolve,reject)=>{
-        console.log("I am a Promise");
-        // resolve("Success");
-        reject("Error.....");
-        
-    });
+
+
+function asyncFunc1() {
+    const getPromise = () => {
+        return new Promise((resolve, reject) => {
+            console.log("I am a Promise");
+            resolve("Success from asyncFunc1");
+            // reject("Error.....");
+        });
+    };
+    return getPromise(); // Return the promise
 }
 
-let promise = getPromise();
+function asyncFunc2() {
+    const getPromise=()=>{
+        return new Promise((resolve, reject)=>{
+            console.log("I am a Promise");
+            resolve("Success from asyncFunc2");
+        });
+    };
+    return getPromise(); // Return the promise
+}
+
+// let promise=asyncFunc1();
+// console.log("Fetching data1......");
+
 // promise.then((data)=>{
-//     console.log("Promise Fullfilled------>",data);
+//     console.log("Promise fulfilled ------>",data);
+//     console.log("Fetching data2......");
+//     let p2=asyncFunc2();
+//     p2.then((data) => {
+//         console.log("Promise fulfilled ------>",data);
+//     });
 // });
 
-promise.catch((error)=>{
-    console.log("Promise rejected------>",error);
-});
+console.log("Fetching data1......");
+
+asyncFunc1()
+    .then((data) => {
+        console.log("Promise fulfilled ------>",data);
+        console.log("Fetching data2......");
+        return asyncFunc2(); 
+    })
+    .then((data) => {
+        console.log("Promise fulfilled ------>",data);
+    })
+    .catch((error) => {
+        console.log("Promise rejected ------>",error);
+    });
