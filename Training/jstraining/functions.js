9c81196f66618d6e88db1f8f5e74f4179381077a
CharanSuggala26 <suggalasaicharan789@gmail.com>
2025-07-20 21:45:01 +0530

    Incorporated corrections

--- Training/jstraining/functions.js
+++ Training/jstraining/functions.js
@@ -10,3 +10,20 @@
 //     });
 // }
 
+const getPromise = ()=>{
+    return new Promise((resolve,reject)=>{
+        console.log("I am a Promise");
+        // resolve("Success");
+        reject("Error.....");
+        
+    });
+}
+
+let promise = getPromise();
+// promise.then((data)=>{
+//     console.log("Promise Fullfilled------>",data);
+// });
+
+promise.catch((error)=>{
+    console.log("Promise rejected------>",error);
+});
